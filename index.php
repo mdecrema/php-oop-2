7405e8f6d669c8157c65282acce2bc174a890216
marco <marcodecrema@libero.it>
2020-10-23 16:03:43 +0200

    class auto

--- index.php
+++ index.php
@@ -1,18 +1,48 @@
 <?php
 
-class User {
-  public $name;
-  public $lastname;
-  public $eta;
+class Auto {
+  public $marca;
+  public $modello;
+  public $targa;
+  public $motore;
+  public $colore;
+  public $prezzo;
+  public $sconto;
 
+
+  class Usate extends Auto {
+    public $km;
+
+    public function setSconto($km) {
+      if ($km > 10000) {
+        $this->$sconto = 30;
+      }
+    }
+  }
 
 
 }
 
-$utente1 = new User("Paolo","Rossi","45");
-$utente2 = new User("Mario","Bianchi","38");
-$utente3 = new User("Luca","Verdi","26");
+$auto1 = new auto("Audi","A3","FG304MA","diesel","nero","24000","0");
+$auto2 = new auto("Mercedes","GLX","EF245TB","benzina","bianco","32000","0");
+$auto3 = new auto("BMW","Serie 1","DZ103NA","diesel","grigio","265000","0");
 
-$utenti = [$utente1, $utente2, $utente3];
+$insiemeAuto = [$auto1, $auto2, $auto3];
 
 ?>
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>OOP 2</title>
+  </head>
+  <body>
+    <?php
+    foreach ($insiemeAuto as $auto) {
+
+    echo $auto->$marca;
+  }
+    ?>
+  </body>
+</html>
